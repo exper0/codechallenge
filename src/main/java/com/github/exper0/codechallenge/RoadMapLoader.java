b8c2785274eb1669fb45b3afda920ef1e0038d86
exper0 <aeg.exper0@gmail.com>
2019-02-10 19:04:46 -0500

    1) file loading bug fix (unclosed stream) 2) actuators, build & git info enhancements

--- src/main/java/com/github/exper0/codechallenge/RoadMapLoader.java
+++ src/main/java/com/github/exper0/codechallenge/RoadMapLoader.java
@@ -24,22 +24,23 @@ public class RoadMapLoader {
     }
 
     public RoadMap load(String filename) throws IOException, URISyntaxException {
-        Stream<String> lines = Files.lines(Paths.get(ClassLoader.getSystemResource(filename).toURI()));
-        Holder<Integer> line = new Holder<>(1);
-        SetMultimap<String, String> mm = LinkedHashMultimap.create();
-        lines.forEach(l->{
-            String [] pair = l.split(SEPARATOR);
-            if (pair.length != 2) {
-                LOGGER.error("line {} error: expected 2 comma separate values, got {}. Skipping", line.value, pair.length);
-                return;
-            }
-            if (!StringUtils.hasText(pair[0]) || !StringUtils.hasText(pair[1])) {
-                LOGGER.error("line {} error: values cannot be empty", line.value);
-                return;
-            }
-            mm.put(pair[0].trim().toLowerCase(), pair[1].trim().toLowerCase());
-            ++line.value;
-        });
-        return roadMapFactory.apply(mm);
+        try (Stream<String> lines = Files.lines(Paths.get(ClassLoader.getSystemResource(filename).toURI()))) {
+            Holder<Integer> line = new Holder<>(1);
+            SetMultimap<String, String> mm = LinkedHashMultimap.create();
+            lines.forEach(l -> {
+                String[] pair = l.split(SEPARATOR);
+                if (pair.length != 2) {
+                    LOGGER.error("line {} error: expected 2 comma separate values, got {}. Skipping", line.value, pair.length);
+                    return;
+                }
+                if (!StringUtils.hasText(pair[0]) || !StringUtils.hasText(pair[1])) {
+                    LOGGER.error("line {} error: values cannot be empty", line.value);
+                    return;
+                }
+                mm.put(pair[0].trim().toLowerCase(), pair[1].trim().toLowerCase());
+                ++line.value;
+            });
+            return roadMapFactory.apply(mm);
+        }
     }
 }
